b1db2ec79dc027d0888a20171db7e0a04d00ccb6
Janis Meybohm <meybohm@traum-ferienwohnungen.de>
2019-08-26 12:26:05 +0200

    Move getLocalAction to service.go
    No need for an extra file plus that gives log messages a clear context.

--- controller/service.go
+++ controller/service.go
@@ -282,6 +282,52 @@ func (c *ServiceController) syncHandler(key string) (ActionType, error) {
 	return ActionTypeNone, fmt.Errorf("something wired happened in service syncHandler")
 }
 
+// getLocalAction returns the type of action (ActionType) to take on local service
+func getLocalAction(remoteExists bool, remoteSvc *v1.Service, localExists bool, localSvc *v1.Service) ActionType {
+	if remoteExists && utils.ResponsibleForService(remoteSvc) {
+		klog.Infof("remote: %s/%s I'm responsible", remoteSvc.GetNamespace(), remoteSvc.GetName())
+
+		if localExists {
+			if !utils.OwnerOfService(localSvc) {
+				klog.Warningf("local: %s/%s I don't own this service, SKIP", localSvc.GetNamespace(), localSvc.GetName())
+				return ActionTypeNone
+			}
+
+			if !utils.ResponsibleForService(localSvc) {
+				klog.Warningf("local: %s/%s not responsible for service, SKIP", localSvc.GetNamespace(), localSvc.GetName())
+				return ActionTypeNone
+			}
+
+			klog.Infof("remote,local: %s/%s both exist, UPDATE", localSvc.GetNamespace(), localSvc.GetName())
+			return ActionTypeUpdate
+		} else {
+			klog.Infof("local: %s/%s does not exist, ADD", remoteSvc.GetNamespace(), remoteSvc.GetName())
+			return ActionTypeAdd
+		}
+	}
+
+	if !remoteExists || !utils.ResponsibleForService(remoteSvc) {
+		// It's not completely sure that remoteSvc is not nil, so we can't log namespace and name
+		klog.Infoln("remote: not responsible")
+
+		if localExists {
+			if !utils.OwnerOfService(localSvc) {
+				klog.Warningf("local: %s/%s I don't own this service, SKIP", localSvc.GetNamespace(), localSvc.GetName())
+				return ActionTypeNone
+			}
+
+			if !utils.ResponsibleForService(localSvc) {
+				klog.Infof("local: %s/%s exists but not responsible, SKIP", localSvc.GetNamespace(), localSvc.GetName())
+				return ActionTypeNone
+			}
+			klog.Infof("local: %s/%s does exist, DELETE", localSvc.GetNamespace(), localSvc.GetName())
+			return ActionTypeDelete
+		}
+	}
+
+	return ActionTypeNone
+}
+
 // enqueueService adds a service (key) to the queue
 func (c *ServiceController) enqueueService(obj interface{}) {
 	key, err := cache.MetaNamespaceKeyFunc(obj)
--- controller/service_test.go
+++ controller/service_test.go
@@ -212,3 +212,143 @@ func TestDeleteService(t *testing.T) {
 	f.expectDeleteServiceAction(localService)
 	f.run(getKey(remoteService, t))
 }
+
+func TestGetLocalAction(t *testing.T) {
+	type testPair struct {
+		remoteExists, localExists bool
+		remote, local             *v1.Service
+		output                    ActionType
+	}
+	var tests = []testPair{
+		{
+			remoteExists: false,
+			remote:       &v1.Service{},
+			localExists:  false,
+			local:        &v1.Service{},
+			output:       ActionTypeNone,
+		},
+		{
+			remoteExists: true,
+			remote: &v1.Service{
+				ObjectMeta: metaV1.ObjectMeta{},
+				Spec: v1.ServiceSpec{
+					Type: v1.ServiceTypeNodePort,
+				},
+			},
+			localExists: false,
+			local:       &v1.Service{},
+			output:      ActionTypeAdd,
+		},
+		{
+			remoteExists: true,
+			remote: &v1.Service{
+				ObjectMeta: metaV1.ObjectMeta{},
+				Spec: v1.ServiceSpec{
+					Type: v1.ServiceTypeNodePort,
+				},
+			},
+			localExists: true,
+			local: &v1.Service{
+				ObjectMeta: metaV1.ObjectMeta{},
+				Spec: v1.ServiceSpec{
+					Type: v1.ServiceTypeNodePort,
+				},
+			},
+			output: ActionTypeNone,
+		},
+		{
+			remoteExists: true,
+			remote: &v1.Service{
+				ObjectMeta: metaV1.ObjectMeta{},
+				Spec: v1.ServiceSpec{
+					Type: v1.ServiceTypeNodePort,
+				},
+			},
+			localExists: true,
+			local: &v1.Service{
+				ObjectMeta: metaV1.ObjectMeta{
+					Labels: utils.ResourceLabel,
+				},
+				Spec: v1.ServiceSpec{
+					Type: v1.ServiceTypeLoadBalancer,
+				},
+			},
+			output: ActionTypeNone,
+		},
+		{
+			remoteExists: true,
+			remote: &v1.Service{
+				ObjectMeta: metaV1.ObjectMeta{},
+				Spec: v1.ServiceSpec{
+					Type: v1.ServiceTypeNodePort,
+				},
+			},
+			localExists: true,
+			local: &v1.Service{
+				ObjectMeta: metaV1.ObjectMeta{
+					Labels: utils.ResourceLabel,
+				},
+				Spec: v1.ServiceSpec{
+					Type: v1.ServiceTypeNodePort,
+				},
+			},
+			output: ActionTypeUpdate,
+		},
+
+		{
+			remoteExists: true,
+			remote: &v1.Service{
+				ObjectMeta: metaV1.ObjectMeta{
+					Annotations: utils.IgnoreAnnotation,
+				},
+				Spec: v1.ServiceSpec{
+					Type: v1.ServiceTypeNodePort,
+				},
+			},
+			localExists: true,
+			local: &v1.Service{
+				ObjectMeta: metaV1.ObjectMeta{},
+				Spec: v1.ServiceSpec{
+					Type: v1.ServiceTypeNodePort,
+				},
+			},
+			output: ActionTypeNone,
+		},
+		{
+			remoteExists: false,
+			remote:       &v1.Service{},
+			localExists:  true,
+			local: &v1.Service{
+				ObjectMeta: metaV1.ObjectMeta{
+					Labels:      utils.ResourceLabel,
+					Annotations: utils.IgnoreAnnotation,
+				},
+				Spec: v1.ServiceSpec{
+					Type: v1.ServiceTypeNodePort,
+				},
+			},
+			output: ActionTypeNone,
+		},
+		{
+			remoteExists: false,
+			remote:       &v1.Service{},
+			localExists:  true,
+			local: &v1.Service{
+				ObjectMeta: metaV1.ObjectMeta{
+					Labels: utils.ResourceLabel,
+				},
+				Spec: v1.ServiceSpec{
+					Type: v1.ServiceTypeNodePort,
+				},
+			},
+			output: ActionTypeDelete,
+		},
+	}
+
+	for n, test := range tests {
+		output := getLocalAction(test.remoteExists, test.remote, test.localExists, test.local)
+		if output != test.output {
+			t.Errorf("Expected '%s' got '%s' for testset #%d", test.output, output, n)
+		}
+	}
+}
